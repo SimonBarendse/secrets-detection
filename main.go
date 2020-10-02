911a2295c1ca6bf086ffb8a780f1020ded714e52
Floris van der Grinten <floris@grinten.com>
2020-10-02 15:25:22 +0200

    Create project with fictional secrets

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Fictional secret
+	apiKey := "hLYuNm4Wzn2RphLYuNms4rhlpse4rhlps"
+	fmt.Println(apiKey)
+}
---
+++ script.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Fictional secret
+api_key="hLYuNm4Wzn2RphLYuNms4rhlpse4rhlps"
+echo $api_key
